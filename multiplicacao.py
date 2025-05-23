b8b81c517c354408a48272efaa24b33bd61756c0
LuisBonete <159036040+LuisBonete@users.noreply.github.com>
2025-05-23 11:40:38 -0300

    Create multiplicacao.py

---
+++ multiplicacao.py
@@ -0,0 +1,13 @@
+tabuada= 0
+dia = float(input("digite o dia do seu aniversari: "))
+print(f"\nContando a tabuada do {dia}  até o 100 com while.")
+
+      
+    while contador <= 100:
+     multiplicacao = dia * contador 
+      contador +=1
+    if  
+    multiplicacao >1000:
+        print(f" este {multiplicacao} e maior que 1000!")
+        print(multiplicacao)
+    
