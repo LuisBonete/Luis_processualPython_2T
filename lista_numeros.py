92366a5a43a13632217c1737f043378e79daa4e4
LuisBonete <159036040+LuisBonete@users.noreply.github.com>
2025-05-28 10:07:27 -0300

    Update lista_numeros.py

--- lista_numeros.py
+++ lista_numeros.py
@@ -1,57 +1,84 @@
 numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '100']
 
-print("--- Análise de Números ---")
+print("--- Análise de Números de 1 a 100 ---")
 for i in range(1, 101):
-    if i == 2:
-        
-        print(f"{i} é primo")
-    if i == 3:
-        
-        print(f"{i} é primo")
+    impresso_como_primo = False
 
+    if i == 2:
+        print(f"{i} é primo")
+        impresso_como_primo = True
+    if i == 3:
+        print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 5:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 7:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 11:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 13:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 17:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 19:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 23:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 29:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 31:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 37:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 41:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 43:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 47:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 53:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 59:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 61:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 67:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 71:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 73:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 79:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 83:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 89:
         print(f"{i} é primo")
+        impresso_como_primo = True
     if i == 97:
         print(f"{i} é primo")
+        impresso_como_primo = True
+
+    if not impresso_como_primo:
+        print(i)
