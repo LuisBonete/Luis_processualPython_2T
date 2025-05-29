d543a3ec8dc61d6adf219802dca130cb8f83c6dd
LuisBonete <159036040+LuisBonete@users.noreply.github.com>
2025-05-29 10:35:50 -0300

    Update nomes.py

--- nomes.py
+++ nomes.py
@@ -9,8 +9,28 @@ nome8 = input("insira o seu nome aqui: ")
 nome9 = input("insira o seu nome aqui: ")
 nome10 = input("insira o seu nome aqui: ")
 
-lista=[nome1, nome2, nome3, nome4, nome5, nome6, nome7, nome8 , nome9, nome10]
+caracteres_invalidos = ["!", "@", ",", "#", "(", ")", "|", "$"]
+
+lista = [nome1, nome2, nome3, nome4, nome5, nome6, nome7, nome8, nome9, nome10]
 lista.sort()
-numeros= len(nome1, nome2, nome3, nome4, nome5, nome6, nome7, nome8 , nome9, nome10)
-print(lista)
-print(numeros)
+
+encontrou_invalido_geral = False
+
+for nome_na_lista in lista:
+    encontrou_invalido_no_nome = False
+    for caractere_invalido in caracteres_invalidos:
+        if caractere_invalido in nome_na_lista:
+            print("Caracteres invalidos encontrados no nome:", nome_na_lista,)
+            encontrou_invalido_no_nome = True
+            encontrou_invalido_geral = True
+            break
+    
+    if encontrou_invalido_geral:
+        break
+
+if not encontrou_invalido_geral:
+    print("\nNomes na lista (ordenados) e contagem de caracteres:")
+    for nome in lista:
+        
+        print(f"- '{nome}': {len(nome)} caracteres")
+    
